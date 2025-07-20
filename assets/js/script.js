09e3282e1a8ad6b46edb381373d8558f35304b4f
declanwatts97 <declan.watts97@gmail.com>
2025-07-20 20:18:19 +0100

    added division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ if(gameType === "addition") {
     displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -84,6 +86,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -129,8 +133,8 @@ document.getElementById('operand2').textContent = operand2;
 document.getElementById('operator').textContent = "x";
 }
 
-function displayMultiplyQuestion(operand1, operand2) {
-document.getElementById('operand1').textContent = operand1;
+function displayDivisionQuestion(operand1, operand2) {
+document.getElementById('operand1').textContent = operand1 * operand2;
 document.getElementById('operand2').textContent = operand2;
-document.getElementById('operator').textContent = "x";
+document.getElementById('operator').textContent = "/";
 }
