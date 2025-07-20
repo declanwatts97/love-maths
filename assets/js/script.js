9253de6ef55bb698f39002f20b57e0c0a310cdab
declanwatts97 <declan.watts97@gmail.com>
2025-07-20 20:00:52 +0100

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ if(gameType === "addition") {
     displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -71,6 +73,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if(operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if(operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -104,8 +108,16 @@ document.getElementById('operand2').textContent = operand2;
 document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1, operand2){
+document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+document.getElementById('operator').textContent = "-";
+}
 
+function displayMultiplyQuestion(operand1, operand2) {
+document.getElementById('operand1').textContent = operand1;
+document.getElementById('operand2').textContent = operand2;
+document.getElementById('operator').textContent = "x";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
